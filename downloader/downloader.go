bc8abc49051dc618781074f20d8222167774fdf1
Kaoru HAYAMA <iyahaya@nifty.com>
2016-09-11 08:50:36 +0900

    downloader: returns errorlevel

--- downloader/downloader.go
+++ downloader/downloader.go
@@ -9,21 +9,18 @@ import (
 
 const BYTES_PER_DOT = 1024 * 1024
 
-func main() {
+func main_() error {
 	if len(os.Args) < 3 {
-		fmt.Fprintf(os.Stderr, "Usage: %s URL FILENAME\n", os.Args[0])
-		return
+		return fmt.Errorf("Usage: %s URL FILENAME", os.Args[0])
 	}
 	res, err := http.Get(os.Args[1])
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[1], err.Error())
-		return
+		return fmt.Errorf("%s: %s", os.Args[1], err.Error())
 	}
 	defer res.Body.Close()
 	w, err2 := os.Create(os.Args[2])
 	if err2 != nil {
-		fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[2], err2.Error())
-		return
+		return fmt.Errorf("%s: %s", os.Args[2], err.Error())
 	}
 	defer w.Close()
 	for {
@@ -31,10 +28,19 @@ func main() {
 		if err3 != nil {
 			fmt.Fprint(os.Stderr, "\n")
 			if err3 != io.EOF {
-				fmt.Fprintln(os.Stderr, err3.Error())
+				return err3
 			}
-			return
+			return nil
 		}
 		fmt.Fprint(os.Stderr, ".")
+	}
+}
+
+func main() {
+	if err := main_(); err != nil && err != io.EOF {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	} else {
+		os.Exit(0)
 	}
 }
