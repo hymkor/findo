95bcef1562ec146bb6edba01f74abd1547e5ab97
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-09-30 11:52:30 +0900

    Add code to learn about https://github.com/Luxurioust/excelize
    memo: manual https://godoc.org/github.com/Luxurioust/excelize

---
+++ exam_excelize/exam.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Luxurioust/excelize"
+)
+
+func main() {
+	for _, arg1 := range os.Args[1:] {
+		xlsx, err := excelize.OpenFile(arg1)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			continue
+		}
+		cell := xlsx.GetCellValue("Sheet1", "A1")
+		fmt.Println(cell)
+	}
+}
