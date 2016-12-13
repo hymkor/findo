d673d457c46ca5796ec79e302107df53d1b2d311
Kaoru HAYAMA <iyahaya@nifty.com>
2016-12-13 17:53:21 +0900

    Add `findo`

---
+++ findo.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+)
+
+func Main() error {
+	var rx *regexp.Regexp
+	if len(os.Args) >= 2 {
+		var err error
+		rx, err = regexp.Compile(os.Args[1])
+		if err != nil {
+			return err
+		}
+	}
+
+	filepath.Walk(".", func(path_ string, info_ os.FileInfo, err_ error) error {
+		name := filepath.Base(path_)
+		if name == "." || name == ".." {
+			return nil
+		}
+		if rx == nil || rx.MatchString(name) {
+			fmt.Println(name)
+		}
+		return nil
+	})
+	return nil
+}
+
+func main() {
+	if err := Main(); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+	}
+}
