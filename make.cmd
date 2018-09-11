66ba41321f33914742e5bc3f9288c8397deb672b
Kaoru HAYAMA <iyahaya@nifty.com>
2018-09-12 02:50:36 +0900

    make.cmd: support make package

--- make.cmd
+++ make.cmd
@@ -1,5 +1,18 @@
 setlocal
-set GOARCH=386
-go fmt
-go build
+call :"%1"
 endlocal
+exit /b
+
+:""
+:"all"
+    go fmt
+    for %%I in (386 amd64) do (
+        set GOARCH=%%I
+        mkdir cmd\%%I
+        go build -o cmd\%%I\findo.exe -ldflags "-s -w"
+    )
+    exit /b
+
+:"package"
+    for %%I in (386 amd64) do zip -j findo-%%I-%DATE:/=%.zip cmd\%%I\findo.exe
+    exit /b
